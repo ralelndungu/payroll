1badfd4c6cbe04c2450d7243558e65fa5867fee8
ralelndungu <98586380+ralelndungu@users.noreply.github.com>
2022-03-27 08:55:37 +0300

    Add files via upload

---
+++ payroll/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char name[100],[20];
+    int hours,overtime,gross,tax,net;
+    printf("Gitonga\'s Factory Payroll\n");
+    //capture inputs
+    return 0;
+}
